5f94c420c361f7832094ab6730b77b1c22151a29
TheSouperSoup <ptrschmidt@icloud.com>
2026-05-12 14:18:43 -0700

    Added Base Files
    Files from work before inserting files to Github

---
+++ ptrschmidtwebpage/JS/scripts.js
@@ -0,0 +1,13 @@
+const navbar = `
+  <nav class="menu">
+            <div class="name">Peter Schmidt</div>
+            <div class="menuItems">
+                <ul>
+                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#About">About</a></li>
+                    <li><a href="#Contact">Contact</a></li>
+                </ul>
+            </div>
+    </nav>
+`;
+document.body.insertAdjacentHTML("afterbegin", navbar);
